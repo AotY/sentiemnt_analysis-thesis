ee2686fdf9b144cb9d7d47044e7230b6e1db5edf
AotY <694731929@qq.com>
2019-03-12 16:22:47 +0800

    augment

--- augment.sh
+++ augment.sh
@@ -10,8 +10,9 @@
 python misc/augment.py \
     --data_path ./data/label.cleaned.txt \
     --save_path ./data/label.augment.cleaned.txt \
-    --syno_path ./data/syno.txt \
-    --augment_num 2 \
+    --syno_path ./data/merge_syno.txt \
+    --cilin_path ./data/cilin_ex.txt \
+    --augment_num 3 \
     --augment_labels 1 \
 
 
