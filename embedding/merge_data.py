dea1ae5d12b51bea736c35f2ad4dce740cc82974
AotY <694731929@qq.com>
2019-03-15 17:06:30 +0800

    word2vec, merge data

---
+++ embedding/merge_data.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright © 2019 LeonTao
+#
+# Distributed under terms of the MIT license.
+
+"""
+Merge data from different sources:
+    https://github.com/SophonPlus/ChineseNlpCorpus
+"""
+
+
+import os
+import argparse
+import pandas as pd
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--data_dir', type=str, default='')
+parser.add_argument('--save_path', type=str, default='')
+args = parser.parse_args()
+
+
+data_sources = [
+    'ChnSentiCorp_htl_all.csv',  # 7000 多条酒店评论数据，5000 多条正向评论，2000 多条负向评论
+    'waimai_10k.csv',  # 某外卖平台收集的用户评价，正向 4000 条，负向 约 8000 条
+    'online_shopping_10_cats.csv',  # 10 个类别，共 6 万多条评论数据，正、负向评论各约 3 万条，
+    'weibo_senti_100k.csv',  # 10 万多条，带情感标注 新浪微博，正负向评论约各 5 万条
+    'simplifyweibo_4_moods.csv',  # 0 万多条，带情感标注 新浪微博，正负向评论约各 5 万条
+    'dmsc_v2/ratings.csv',  # 28 部电影，超 70 万 用户，超 200 万条 评分/评论 数据
+    'yf_dianping/ratings.csv',  # 24 万家餐馆，54 万用户，440 万条评论/评分数据
+    'yf_amazon/ratings.csv',  # 52 万件商品，1100 多个类目，142 万用户，720 万条评论/评分数据
+    'raw.question.txt',
+    #  'ez_douban/ratings.csv',  # 5 万多部电影（3 万多有电影名称，2 万多没有电影名称），2.8 万 用户，280 万条评分数据
+]
+
+save_file = open(args.save_path, 'w', encoding='utf-8')
+for source in data_sources:
+    data_path = os.path.join(args.data_dir, source)
+
+    if source.endswith('csv'):
+        df = pd.read_csv(data_path)
+        df = df.dropna(how='any')
+    if source.startswith('dmsc_v2'):
+        texts = df['comment']
+    elif source.startswith('yf_dianping'):
+        texts = df['comment']
+    elif source.startswith('yf_amazon'):
+        texts = df['comment']
+    elif source.startswith('raw.question.txt'):
+        texts = list()
+        print('Loading raw.question.txt...')
+        with open(data_path, 'r') as f:
+            for line in tqdm(f):
+                line = line.rstrip()
+                try:
+                    q_id, d_id, title, query, response, \
+                        sub, gender, age, onset, labels = line.split('SPLIT')
+                except ValueError as e:
+                    continue
+                texts.append(response)
+        pass
+    else:
+        texts = df['review']
+        pass
+
+    print('Save text...')
+    for text in tqdm(texts):
+        #  print(text)
+        if len(text) >= 5:
+            save_file.write('%s\n' % text.rstrip())
+    texts = None
+
+save_file.close()
---
+++ embedding/word2vec/scripts/train_word2vec.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+# train_word2vec.sh
+# Copyright (C) 2019 LeonTao
+#
+# Distributed under terms of the MIT license.
+#
+
+DATA_DIR=./data
+BIN_DIR=./bin
+SRC_DIR=./src
+
+TEXT_DATA=$DATA_DIR/merged_data.txt
+# VECTOR_DATA=$DATA_DIR/merged_data.vec.bin
+VECTOR_DATA=$DATA_DIR/merged_data.vec.txt
+
+pushd ${SRC_DIR} && make; popd
+
+if [ ! -e $VECTOR_DATA ]; then
+    echo -----------------------------------------------------------------------------------------------------
+    echo -- Training vectors...
+    time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 1 -size 128 -window 5 -negative 5 -hs 0 -sample 1e-3 -threads 12 -binary 0
+
+fi
+
+echo -----------------------------------------------------------------------------------------------------
+echo -- distance...
+
+$BIN_DIR/distance $DATA_DIR/$VECTOR_DATA
+
