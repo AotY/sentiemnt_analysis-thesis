16cf5cfe65dba20bb7c3755450dd93b653bf12e5
AotY <694731929@qq.com>
2019-03-20 00:20:15 +0800

    word2vec, pinyin

--- embedding/word2vec/scripts/train_word2vec.sh
+++ embedding/word2vec/scripts/train_word2vec.sh
@@ -10,21 +10,24 @@ DATA_DIR=./data
 BIN_DIR=./bin
 SRC_DIR=./src
 
-TEXT_DATA=$DATA_DIR/merged_data.txt
-# VECTOR_DATA=$DATA_DIR/merged_data.vec.bin
-VECTOR_DATA=$DATA_DIR/merged_data.vec.txt
+WORD_DATA=$DATA_DIR/merged_data.txt
+PINYIN_DATA=$DATA_DIR/merged_data.pinyin.txt
+# VECTOR_PATH=$DATA_DIR/merged_data.vec.bin
+VECTOR_PATH=$DATA_DIR/merged_data.vec.txt
+VOCAB_PATH=$DATA_DIR/merged_data.vocab.txt
+PINYIN_VOCAB_PATH=$DATA_DIR/merged_data.pinyin.vocab.txt
 
 #pushd ${SRC_DIR} && make; popd
 
-#if [ ! -e $VECTOR_DATA ]; then
-    #echo -----------------------------------------------------------------------------------------------------
-    #echo -- Training vectors...
-    #time $BIN_DIR/word2vec -train $TEXT_DATA -output $VECTOR_DATA -cbow 1 -size 128 -window 5 -negative 5 -hs 0 -sample 1e-3 -threads 12 -binary 0
+if [ ! -e $VECTOR_PATH ]; then
+    echo -----------------------------------------------------------------------------------------------------
+    echo -- Training vectors...
+    time $BIN_DIR/word2vec -train-word $WORD_DATA -output $VECTOR_PATH -cbow 1 -size 128 -window 5 -negative 5 -hs 0 -sample 1e-3 -threads 12 -binary 0 -save-vocab $VOCAB_PATH -save-pinyin-vocab $PINYIN_VOCAB_PATH -model-type 1 -train-pinyin $PINYIN_DATA
 
-#fi
+fi
 
 #echo -----------------------------------------------------------------------------------------------------
 #echo -- distance...
 
-$BIN_DIR/distance $VECTOR_DATA
+# $BIN_DIR/distance $VECTOR_PATH
 
