43895c52f01c7ce5fade864616e792d2f4f67ad6
AotY <694731929@qq.com>
2019-03-20 22:01:59 +0800

    train.sh

--- embedding/word2vec/scripts/train.sh
+++ embedding/word2vec/scripts/train.sh
@@ -11,8 +11,8 @@ DATA_DIR=./data
 BIN_DIR=./bin
 SRC_DIR=./src
 
-MODEL_TYPE=1
-CBOW=0
+MODEL_TYPE=2
+CBOW=1
 NEGATIVE=5
 WINDOW=5
 BINARY=1
@@ -20,7 +20,7 @@ SIZE=128
 
 TRAIN_WORD=$DATA_DIR/merged_data.txt
 TRAIN_PINYIN=$DATA_DIR/merged_data.pinyin.txt
-VECTOR_PATH=$DATA_DIR/merged_data.vec.1_sg_negative_128.bin
+VECTOR_PATH=$DATA_DIR/merged_data.vec.2.cbow_negative_128.bin
 
 
 ./bin/word2vec -train-word ./data/merged_data.txt -train-pinyin $TRAIN_PINYIN -output $VECTOR_PATH -size $SIZE -binary $BINARY -cbow $CBOW -window $WINDOW -debug 2 -negative $NEGATIVE -threads 12 -min-count 5 -model-type $MODEL_TYPE
